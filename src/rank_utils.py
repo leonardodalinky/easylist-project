720e2b5822181c758a8ff255c15534c5418748f8
leonardodalinky <493987054@qq.com>
2022-10-30 18:16:35 +0800

    feat: diff.ipynb & rank utils updated

--- src/rank_utils.py
+++ src/rank_utils.py
@@ -13,3 +13,10 @@ DEFAULT_MAJESTIC_PATH = PROJECT_DIR / ".cache" / "download" / "majestic_million.
 def load_majestic(path: Path = DEFAULT_MAJESTIC_PATH) -> pd.DataFrame:
     assert path.exists(), f"Majestic file does not exist: {path}. Try to run `scripts/download_majestic.py` first."
     return pd.read_csv(path)
+
+
+def find_majestic_rank_entry_by_domain(domain: str, majestic_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Find the entry of the specified domain in majestic million.
+    """
+    return majestic_df[majestic_df["Domain"] == domain]
